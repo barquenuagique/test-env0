bd1c970cd4f8afea015d105fc45fd38f47702ed9
barquenuagique <61878800+barquenuagique@users.noreply.github.com>
2020-03-29 11:32:49 +0200

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,20 @@
+
+resource "aws_instance" "vm" {
+  ami                    = data.aws_ami.amazon-linux.id
+  instance_type          = var.instance_type
+  tags = {
+    Name = " VM DESTROY ME - ${terraform.workspace}",
+    Terraform = "true"
+  }
+}
+
+data "aws_ami" "amazon-linux" {
+  most_recent = true
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm-*"]
+  }
+
+}
